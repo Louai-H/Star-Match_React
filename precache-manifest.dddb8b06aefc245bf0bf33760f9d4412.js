2904abdbcaed74e8168cdb652dcd7a10589004ae
Louai Hamada <hamada.loai@gmail.com>
2020-05-11 04:41:41 +0300

    Updates

---
+++ precache-manifest.dddb8b06aefc245bf0bf33760f9d4412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b062b7ab8c9c7a6dcb861afb089350",
+    "url": "/Star-Match_React/index.html"
+  },
+  {
+    "revision": "bb689e15715a0e884b0e",
+    "url": "/Star-Match_React/static/css/main.55fa3c39.chunk.css"
+  },
+  {
+    "revision": "e2876e94349e6adb0a35",
+    "url": "/Star-Match_React/static/js/2.53848e47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Star-Match_React/static/js/2.53848e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb689e15715a0e884b0e",
+    "url": "/Star-Match_React/static/js/main.da58dabb.chunk.js"
+  },
+  {
+    "revision": "f36efc43806ca4ea5786",
+    "url": "/Star-Match_React/static/js/runtime-main.fea9c0a0.js"
+  }
+]);
